b17879edbe28ed078b8f248dad5989b0161ca60b
Cyril Annette <zougouda@gmail.com>
2016-05-03 16:08:00 +0200

    Unload actions & Better code organization

--- src/main.c
+++ src/main.c
@@ -9,6 +9,33 @@ static int s_battery_level;
 
 static GBitmap *ship_bitmap;
 
+static void battery_callback(BatteryChargeState state) 
+{
+  s_battery_level = state.charge_percent;
+  layer_mark_dirty(s_battery_layer);
+}
+
+static void update_time() 
+{
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), 
+           clock_is_24h_style() ? "%H:%M" : "%I:%M", 
+           tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) 
+{
+  update_time();
+}
+
 static void battery_update_proc(Layer *layer, GContext *ctx) 
 {
   GRect bounds = layer_get_bounds(layer);
@@ -49,72 +76,51 @@ static void main_window_load(Window *window)
   GRect bounds = layer_get_bounds(window_layer);  
   GPoint center = grect_center_point(&bounds);
   
+  // Ship layer
   ship_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ship_1);
   GSize image_size = gbitmap_get_bounds(ship_bitmap).size;
   GRect image_frame = GRect(center.x, center.y, image_size.w, image_size.h);
   image_frame.origin.y += 10;
   image_frame.origin.x -= image_size.w / 2;
   image_frame.origin.y -= image_size.h / 2;
-  
-  // Ship layer
-  s_ship_layer = bitmap_layer_create(
-      image_frame
-  );
+  s_ship_layer = bitmap_layer_create(image_frame);
   bitmap_layer_set_background_color(s_ship_layer, GColorClear);
   bitmap_layer_set_compositing_mode(s_ship_layer, GCompOpSet);
   bitmap_layer_set_bitmap(s_ship_layer, ship_bitmap);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_ship_layer));
-  
   
   // Battery layer
   s_battery_layer = layer_create(GRect(image_frame.origin.x, image_frame.origin.y + image_size.h + 10, image_size.w, 8));
   layer_set_update_proc(s_battery_layer, battery_update_proc);
-  layer_add_child(window_layer, s_battery_layer);
-  
   
   // Time layer
-  s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(0, 0), bounds.size.w, 40)
-  );
+  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(0, 0), bounds.size.w, 40));
   text_layer_set_background_color(s_time_layer, GColorBlack);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_32_BOLD_NUMBERS));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
+  // create all layers
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_ship_layer));
+  layer_add_child(window_layer, s_battery_layer);
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  
+  update_time(); // time init
+  battery_callback(battery_state_service_peek()); // battery init
+  
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler); // time update
+  battery_state_service_subscribe(battery_callback); // battery update
 }
 
 static void main_window_unload(Window *window) 
 {
+  tick_timer_service_unsubscribe();
+  battery_state_service_unsubscribe();
+  connection_service_unsubscribe();
+  
   layer_destroy(s_battery_layer);
-}
-
-static void battery_callback(BatteryChargeState state) 
-{
-  s_battery_level = state.charge_percent;
-  layer_mark_dirty(s_battery_layer);
-}
-
-static void update_time() 
-{
-  // Get a tm structure
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-
-  // Write the current hours and minutes into a buffer
-  static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), 
-           clock_is_24h_style() ? "%H:%M" : "%I:%M", 
-           tick_time);
-
-  // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, s_buffer);
-}
-
-static void tick_handler(struct tm *tick_time, TimeUnits units_changed) 
-{
-  update_time();
+  text_layer_destroy(s_time_layer);
+  bitmap_layer_destroy(s_ship_layer);
 }
 
 static void init() 
@@ -127,13 +133,7 @@ static void init()
     .unload = main_window_unload
   });
   
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler); // time update
-  battery_state_service_subscribe(battery_callback); // battery update
-  
-  window_stack_push(s_main_window, true);
-  
-  update_time(); // time init
-  battery_callback(battery_state_service_peek()); // battery init
+  window_stack_push(s_main_window, true);  
 }
 
 static void deinit() 
